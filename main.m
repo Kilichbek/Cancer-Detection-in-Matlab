5b5b33d65fec82af0d9dc926766d9a15f8da612a
Kilichbek Haydarov <kilich95@gmail.com>
2019-06-08 09:43:37 +0900

    Make some changes

--- main.m
+++ main.m
@@ -1,14 +1,14 @@
 
 clc; clear all; close all; % Tidy up
-
+addpath('Feature Extraction','Feature Selection');
 %% Feature Extraction Stage
 train_dir = './images/train/';
 valid_dir = './images/validation/';
 test_dir = './images/test/';
 
-[trainX, trainY] = extract_features(train_dir);
-[validX, validY] = extract_features(valid_dir); 
-[testX, testY] = extract_features(valid_dir); 
+[trainX, trainY,trainIDs] = extract_features(train_dir);
+[validX, validY,validIDs] = extract_features(valid_dir); 
+[testX, testY,testIDs] = extract_features(test_dir); 
 
 train_matrix = [trainX, trainY'];
 valid_matrix = [validX, validY'];
@@ -18,11 +18,20 @@ save train_features.mat train_matrix;
 save valid_features.mat valid_matrix;
 save test_features.mat test_matrix;
 
+save train_ids trainIDs;
+save valid_ids validIDs;
+save test_ids testIDs;
+
 
 %% Normalization
 load train_features.mat;
 load valid_features.mat;
 load test_features.mat;
+
+load train_ids trainIDs;
+load valid_ids validIDs;
+load test_ids testIDs;
+
 
 % columns to be normalized
 cols = [1,2,5,7,8,10,11,15];
@@ -91,14 +100,20 @@ Xd = coeff(:,1:floor(n/2))' * Z';
 features = Xd';
 
 [labels,post_probs] = predict(score_model,features);
+neg = post_probs(:,1);
+pos = post_probs(:,2);
 
-ids = [];
-for i=1:m
-    str = sprintf("ts%d",i);
-    ids = [ids,str];
+temp = char(testIDs);
+for i=1:numel(testIDs)
+    cell_arr{i} = temp(:,:,i);
 end
+[~,index] = natsort(cell_arr);
+neg = neg(index);
+pos = pos(index);
+ids = convertCharsToStrings(cell_arr);
+ids = ids(index);
 
-T = table(ids',labels,post_probs(:,1),post_probs(:,2),'VariableNames',...
+T = table(ids',labels,neg,pos,'VariableNames',...
     {'ID','Predictions','B','C'})
 
 fname='PR_result_16012676.csv';
